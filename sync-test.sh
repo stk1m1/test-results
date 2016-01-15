8df63b1dce2e4f9b27f969eb719ce01c457b1a59
SeongTaekC <ssun_k@yahoo.com>
2016-01-15 14:49:15 +0900

    update

--- sync-test.sh
+++ sync-test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 rsync -avz ../realm-java/realm/realm-library/build/reports/androidTests/connected/* .
+git add --all .
+git commit -m "update"
+git push realm-github:stk1m1/test-results.git
